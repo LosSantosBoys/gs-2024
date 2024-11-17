b51c79b4ddc460ef38cbe9afa71b301daf4efb6d
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 17:08:54 -0300

    Adiciona rota de dispositivos

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,15 @@
+import 'package:app/app/features/devices/devices_module.dart';
+import 'package:app/app/features/devices/store/device_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addSingleton<DeviceStore>(DeviceStore.new);
+  }
 
   @override
-  void routes(r) {}
+  void routes(r) {
+    r.module('/', module: DevicesModule());
+  }
 }
