788f3c654c123e3609c5d6d6524d7b8116b6f769
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 18:18:45 -0300

    Add `PriorityLevel`

--- lib/app/core/util.dart
+++ lib/app/core/util.dart
@@ -1,5 +1,7 @@
 import 'package:app/app/core/enum/device_type_enum.dart';
 import 'package:app/app/core/enum/frequency_enum.dart';
+import 'package:app/app/core/enum/priority_level_enum.dart';
+import 'package:flutter/material.dart';
 
 extension DeviceExtension on DeviceTypeEnum {
   String get readable {
@@ -26,6 +28,33 @@ extension DeviceExtension on DeviceTypeEnum {
         return 'Geladeira';
       case DeviceTypeEnum.other:
         return 'Outro';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case DeviceTypeEnum.light:
+        return Icons.lightbulb_outline;
+      case DeviceTypeEnum.airconditioner:
+        return Icons.ac_unit_outlined;
+      case DeviceTypeEnum.tv:
+        return Icons.tv_outlined;
+      case DeviceTypeEnum.refrigerator:
+        return Icons.kitchen_outlined;
+      case DeviceTypeEnum.washingmachine:
+        return Icons.local_laundry_service_outlined;
+      case DeviceTypeEnum.heater:
+        return Icons.whatshot_outlined;
+      case DeviceTypeEnum.fan:
+        return Icons.air_outlined;
+      case DeviceTypeEnum.oven:
+        return Icons.local_pizza_outlined;
+      case DeviceTypeEnum.computer:
+        return Icons.computer_outlined;
+      case DeviceTypeEnum.fridge:
+        return Icons.kitchen_outlined;
+      case DeviceTypeEnum.other:
+        return Icons.devices_other_outlined;
     }
   }
 }
@@ -70,6 +99,47 @@ extension FrequencyExtension on FrequencyEnum {
         return '$times vezes por mês';
       case FrequencyEnum.custom:
         return '$times vezes por $days dias';
+    }
+  }
+}
+
+extension PriorityLevelExtension on PriorityLevelEnum {
+  String get readable {
+    switch (this) {
+      case PriorityLevelEnum.low:
+        return 'Baixa';
+      case PriorityLevelEnum.medium:
+        return 'Média';
+      case PriorityLevelEnum.high:
+        return 'Alta';
+      case PriorityLevelEnum.critical:
+        return 'Crítica';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case PriorityLevelEnum.low:
+        return Icons.arrow_downward_outlined;
+      case PriorityLevelEnum.medium:
+        return Icons.circle_outlined;
+      case PriorityLevelEnum.high:
+        return Icons.arrow_upward_outlined;
+      case PriorityLevelEnum.critical:
+        return Icons.error_outline;
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case PriorityLevelEnum.low:
+        return Colors.green;
+      case PriorityLevelEnum.medium:
+        return Colors.blue;
+      case PriorityLevelEnum.high:
+        return Colors.orange;
+      case PriorityLevelEnum.critical:
+        return Colors.red;
     }
   }
 }
