4d189d2f44689e2233e1bd89f61dca5461cfa6b0
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 19:50:28 -0300

    add const

--- lib/app/features/devices/devices_module.dart
+++ lib/app/features/devices/devices_module.dart
@@ -3,12 +3,13 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 class DevicesModule extends Module {
   @override
-  void binds(Injector i) {}
+  void binds(Injector i) {
+  }
 
   @override
   void routes(r) {
     // TODO: Implementar edição de dispositivo
-    r.child('/', child: (context) => SaveDevicePage());
-    // TODO: Adicionar lista de dispositivos  
+    r.child('/', child: (context) => const SaveDevicePage());
+    // TODO: Adicionar lista de dispositivos
   }
 }
