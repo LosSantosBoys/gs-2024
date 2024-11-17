ddf237fe0114f21acad775d34bc37375a5574332
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 19:50:03 -0300

    Create device_dao.dart

---
+++ lib/app/core/dao/device_dao.dart
@@ -0,0 +1,20 @@
+import 'package:app/app/core/entity/device.dart';
+import 'package:floor/floor.dart';
+
+@dao
+abstract class DeviceDao {
+  @Query('SELECT * FROM Device')
+  Future<List<Device>> findAllDevices();
+
+  @Query('SELECT * FROM Device WHERE id = :id')
+  Future<Device?> findDeviceById(int id);
+
+  @insert
+  Future<void> insertDevice(Device device);
+
+  @update
+  Future<void> updateDevice(Device device);
+
+  @delete
+  Future<void> deleteDevice(Device device);
+}
