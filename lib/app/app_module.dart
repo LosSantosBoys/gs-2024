f1476cecfe31928b1e8b951af660960de0b7d7e6
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 19:50:24 -0300

    add device service

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,10 +1,12 @@
 import 'package:app/app/features/devices/devices_module.dart';
+import 'package:app/app/features/devices/service/device_service.dart';
 import 'package:app/app/features/devices/store/device_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
   void binds(i) {
+    i.addSingleton<DeviceService>(DeviceServiceImpl.new);
     i.addSingleton<DeviceStore>(DeviceStore.new);
   }
 
