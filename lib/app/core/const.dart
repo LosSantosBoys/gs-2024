e8077fe4b0a624fde6345481e8c423d1ce0cad26
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 17:08:36 -0300

    Create const.dart

---
+++ lib/app/core/const.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color primaryDarkest = Color(0xFF3755C1);
+  static const Color primaryDark = Color(0xFF4b67c8);
+  static const Color primaryMedium = Color(0xFF5f77cd);
+  static const Color primaryLight = Color(0xFF8799DA);
+  static const Color primaryLightest = Color(0xFFA8BFFF);
+
+  static const Color neutralLightDarkest = Color(0xFFC5C6CC);
+  static const Color neutralLightDark = Color(0xFFD4D6DD);
+  static const Color neutralLightMedium = Color(0xFFE8E9F1);
+  static const Color neutralLightLight = Color(0xFFF8F9FE);
+  static const Color neutralLightLightest = Color(0xFFFFFFFF);
+
+  static const Color neutralDarkDarkest = Color(0xFF1F2024);
+  static const Color neutralDarkDark = Color(0xFF2F3036);
+  static const Color neutralDarkMedium = Color(0xFF494A50);
+  static const Color neutralDarkLight = Color(0xFF71727A);
+  static const Color neutralDarkLightest = Color(0xFF8F9098);
+
+  static const Color successDark = Color(0xFF288267);
+  static const Color successMedium = Color(0xFF33A885);
+  static const Color successLight = Color(0xFF3ecea3);
+
+  static const Color warningDark = Color(0xFFE86339);
+  static const Color warningMedium = Color(0xFFFFB37C);
+  static const Color warningLight = Color(0xFFFFF4E4);
+
+  static const Color errorDarkest = Color(0xFFE30000);
+  static const Color errorMedium = Color(0xFFFFADB9);
+  static const Color errorLight = Color(0xFFF9C2C3);
+  static const Color errorLightest = Color(0xFFFFF2F4);
+}
