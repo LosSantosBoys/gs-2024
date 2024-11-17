95fa27d31bcd8b116295448b68d6022d73e36155
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-17 19:50:38 -0300

    Create device_service.dart

---
+++ lib/app/features/devices/service/device_service.dart
@@ -0,0 +1,59 @@
+import 'package:app/app/core/database.dart';
+import 'package:app/app/core/entity/device.dart';
+
+abstract class DeviceService {
+  Future<void> saveDevice(Device device);
+  Future<void> deleteDevice(Device device);
+  Future<void> updateDevice(Device device);
+  Future<List<Device>> getDevices();
+}
+
+class DeviceServiceImpl implements DeviceService {
+  @override
+  Future<void> deleteDevice(Device device) async {
+    try {
+      final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+      final deviceDao = database.deviceDao;
+      await deviceDao.deleteDevice(device);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Device>> getDevices() async {
+    try {
+      final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+      final deviceDao = database.deviceDao;
+      return await deviceDao.findAllDevices();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> saveDevice(Device device) async {
+    try {
+      final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+      final deviceDao = database.deviceDao;
+      await deviceDao.insertDevice(device);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> updateDevice(Device device) async {
+    try {
+      final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+      final deviceDao = database.deviceDao;
+      await deviceDao.updateDevice(device);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
