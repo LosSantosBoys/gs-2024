cbcc3290d39188b077966dde49862846b80354cc
Samuel Marques <samuel.s.marques@protonmail.com>
2024-11-21 16:14:30 -0300

    add tariffs and id route

--- lib/app/features/configuration/configuration_module.dart
+++ lib/app/features/configuration/configuration_module.dart
@@ -1,14 +1,17 @@
+import 'package:app/app/features/configuration/pages/add_tariff_page.dart';
+import 'package:app/app/features/configuration/pages/list_tariff_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'pages/configuration_page.dart';
 
 class ConfigurationModule extends Module {
   @override
-  void binds(Injector i) {
-  }
+  void binds(Injector i) {}
 
   @override
   void routes(r) {
     r.child('/', child: (context) => const ConfigurationPage());
+    r.child('/tariffs', child: (context) => const ListTariffsPage());
+    r.child('/:id', child: (context) => SaveTariffPage(id: r.args.params['id']));
   }
 }
